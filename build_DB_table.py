91fa6c13eaa943b3a77f801740f1d07c3a3e3e57
saurabh-chugh2358 <saurabh.chugh0323@gmail.com>
2017-11-01 15:02:55 -0400

    added a column to build reports on tableau

--- build_DB_table.py
+++ build_DB_table.py
@@ -1,27 +1,28 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='<<db_instance_primary_user_name>>',
-                              password='<<db_instance_password>>',
+cnx = mysql.connector.connect(user='G24431890',
+                              password='rohit2358',
                               host='g24431890-mysql.c1lczmt6e8zi.us-east-1.rds.amazonaws.com',
                               database='G24431890_ssanames')
-crsr = cnx.cursor()
-
 
 def load_data(file_path, in_year):
     curr_record = {}
 
     for line in file_path.readlines():
         curr_record = line.split(",")
-        query = """INSERT INTO Names_Frequency(FirstName,Gender,Frequency) 
-                VALUES('%s','%s',%d)""" % (curr_record[0], curr_record[1], int(curr_record[2]), in_year)
-        args = (curr_record[0], curr_record[1], int(curr_record[2]))
+        query = """INSERT INTO Names_Frequency_Data(FirstName,Gender,Frequency,NameYear) 
+                VALUES('%s','%s',%d,%d)""" % (curr_record[0], curr_record[1], int(curr_record[2]), in_year)
+        args = (curr_record[0], curr_record[1], int(curr_record[2]), in_year)
         crsr.execute(query)
         cnx.commit()
+    return
 
+crsr = cnx.cursor()
 
 for x in range(1880, 2017):
-    file_names = open("names_data/yob"+str(x)+".txt", 'r')
-    load_data(file_names, x)
+    names_file = open("/Users/nikhilswami7/Downloads/names/"+"yob"+str(x)+".txt", 'r')
+    print(names_file)
+    load_data(names_file, x)
 
 crsr.close()
 cnx.close()
